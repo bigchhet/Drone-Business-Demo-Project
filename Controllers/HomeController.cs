6d301b010376f85a74b0fff32b6fb96f3fc9400c
Vichhai Chhet <vichhai_chhet562@outlook.com>
2021-11-05 19:31:36 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DroneBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DroneBusiness.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Project_Description()
+        {
+            return View();
+        }
+
+        public IActionResult Studio()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Pilot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DroneBusiness.Models
+{
+    public class Pilot
+    {
+        [Key]
+        public int ID { get; set; }
+        public decimal Wage { get; set; }
+        public int age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CellNumber { get; set; }
+    }
+}
